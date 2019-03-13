daf486545e48304b97eedf553c0232981b3c81f7
RedxLus <luisdieguezbarrio@gmail.com>
2019-03-13 18:52:43 +0100

    Create otro.sh

---
+++ otro.sh
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="es-ES">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="0; url=https://luisiblogdeinformatica.com/category/seguridad/">
+        <script type="text/javascript">
+            window.location.href = "https://luisiblogdeinformatica.com/category/seguridad/"
+        </script>
+        <title>Page Redirection</title>
+    </head>
+    <body>
+        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+        If you are not redirected automatically, follow this <a href='https://luisiblogdeinformatica.com/category/seguridad/'>link to LuisiBlog</a>.
+    </body>
+</html>
